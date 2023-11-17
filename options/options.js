b5fa76fb778b70469728a74a66e8ab3a8d73ed61
Crowdbotics <engineering@crowdbotics.com>
2023-11-17 19:45:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "curtis-finance-tes-129514",
+  url: "https://curtis-finance-tes-129514.botics.co",
+  api: "https://curtis-finance-tes-129514.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
